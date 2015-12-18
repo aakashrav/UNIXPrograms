e9211c30e29246a3c17ab292cd7a042b76ae6893
Aakash Ravi <AakashRavi@Aakashs-MacBook-Pro.local>
2015-12-18 20:49:33 +0100

    slight changes to file_locking

--- file_locks/file_communication.c
+++ file_locks/file_communication.c
@@ -17,6 +17,20 @@
  */
 const char * FILENAME = "random_file";
 
+/* The length of the file in total. This parameter is necessary or else we might
+ * get race conditions on newly written bytes, since we can't lock them. Therefore,
+ * we pre-write all the bytes we need, then lock the parts that we need, rewrite them
+ * and unlock them
+ */
+const int FILE_LEN = 2000;
+
+/* The length of the message we will write. If a process fails to write to the file
+ * because the other process has locked a part of it, we will simply increment our
+ * offset by this size and try and write to the next part!
+ */
+const int message_length = sizeof("Hello from process A!\n");
+
+
 void error(const char * message)
 {
 	perror(message);
@@ -28,7 +42,11 @@ int main(int argc, char * argv[])
 {
 	/* We first unlink the file to start with a clean slate*/
 	unlink(FILENAME);
-	int fd = open(FILENAME, O_CREAT | O_RDWR);
+	int fd = open(FILENAME, O_CREAT | O_RDWR, 0666);
+
+	/* Pre-extend the file length to the total length we need, then just lock the parts
+	 * we require, SEEK_SET allows us to input the desired offset*/
+	(void) lseek(fd, SEEK_SET, FILE_LEN-1);
 
 	pid_t pid;
 	if ((pid = fork()) < 0) 
@@ -38,37 +56,51 @@ int main(int argc, char * argv[])
 
 	if (pid == 0)
 	{
+		/* Open a new file descriptor for the child, so we don't have two file
+		* descriptors for separate processes and avoid hard to debug errors
+		*/
+	 	 int fd2 = open(FILENAME, O_RDWR);
+
+		 if (fd2 < 0)
+		 {
+	 	 	error("Error opening file descriptor child");
+		 }
+
 		for (int i = 0; i < 100; i++)
 		{
+
+			 char buf[] = "Hello from process A!\n";
+
 			/* The child locks from the end of the file onwards.
 			 * At the start, the file is 0 bytes and hence this is 
 			 * equivalent to locking from the beginning onwards
 			 */
 			struct flock f1;
 			f1.l_type = F_WRLCK;
-			f1.l_whence = SEEK_END;
+			f1.l_whence = SEEK_CUR;
 			f1.l_start = 0;
-			f1.l_len = 0;
+			f1.l_len = strlen(buf);
 			f1.l_pid = getpid();
-			if (fcntl(fd, F_SETLKW, &f1) < 0)
+
+			int block_status; 
+
+			while ( (block_status = fcntl(fd2, F_SETLK, &f1)) < 0)
 			{
-				error("Error locking file child!");
+				//Shift the offset and try to write to the next free location
+				(void)lseek(fd2, SEEK_SET, lseek(fd2, 0, SEEK_CUR) + message_length);
 			}
 
-			char buf[] = "Hello from process A!\n";
-			if (write(fd, buf, strlen(buf)) < 0)
+			if (write(fd2, buf, strlen(buf)) < 0)
 			{
 				error("Error writing child process A!");
 			}
-			f1.l_type = F_UNLCK;
 
-			/* We need to unlock the entire length of file that this process has written.
-			 * SEEK_END will only look at the current file end, we need to look back before 
-			 * the process began its writing.
-			 */
+			/* Unlock from (SEEK_CUR - strlen(buf)) to SEEK_CUR*/
 			int negative_offset = strlen(buf) * -1;
 			f1.l_start = negative_offset;
-			if (fcntl(fd, F_SETLK, &f1) < 0)
+			f1.l_type = F_UNLCK;
+
+			if (fcntl(fd2, F_SETLK, &f1) < 0)
 			{
 				error("Error unlocking file child!");
 			}
@@ -79,30 +111,32 @@ int main(int argc, char * argv[])
 		//Same code and logic, but for process B
 		for (int i = 0; i < 100; i++)
 		{
+			char buf[] = "Hello from process B!\n";
+
 			struct flock f1;
 			f1.l_type = F_WRLCK;
-			f1.l_whence = SEEK_END;
+			f1.l_whence = SEEK_CUR;
 			f1.l_start = 0;
-			f1.l_len = 0;
+			f1.l_len = strlen(buf);
 			f1.l_pid = getpid();
-			if (fcntl(fd, F_SETLKW, &f1) < 0)
+
+			int block_status;
+
+			while ( (block_status = fcntl(fd, F_SETLK, &f1)) < 0)
 			{
-				error("Error locking file parent!");
+				//Shift the offset and try to write to the next free location
+				(void)lseek(fd, SEEK_SET, lseek(fd, 0, SEEK_CUR) + message_length);
 			}
 
-			char buf[] = "Hello from process B!\n";
 			if (write(fd, buf, strlen(buf)) < 0)
 			{
 				error("Error writing parent process B!");
 			}
-			f1.l_type = F_UNLCK;
 
-			/* We need to unlock the entire length of file that this process has written.
-			 * SEEK_END will only look at the current file end, we need to look back before 
-			 * the process began its writing.
-			 */
+			/* Unlock from (SEEK_CUR - strlen(buf)) to SEEK_CUR*/
 			int negative_offset = strlen(buf) * -1;
 			f1.l_start = negative_offset;
+			f1.l_type = F_UNLCK;
 			if (fcntl(fd, F_SETLK, &f1) < 0)
 			{
 				error("Error unlocking file parent!");
