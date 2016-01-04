824a5486585ab6fa05805915f680f98284f655c1
Aakash Ravi <AakashRavi@Aakashs-MacBook-Pro.local>
2016-01-04 22:20:51 +0100

    changes to password checker and file_locks

--- dynamic_password_checking/tester.c
+++ dynamic_password_checking/tester.c
@@ -8,6 +8,8 @@
 
 int (*password_check)(const char *);
 
+extern pwq_t password_check_plugin;
+
 void dynliberror(const char * message)
 {
 	printf("%s",dlerror());
@@ -24,63 +26,7 @@ int is_directory(const char * path)
 void 
 load_plugins()
 {
-	char * CWD = getenv("PWD");
-	char * fullpath = calloc(strlen(CWD) + 
-		+ strlen("/lib") + 1,1);
-	strcat(fullpath, CWD);
-	strcat(fullpath, "/lib");
-
-	DIR * libraries;
-	struct dirent *dir;
-	libraries = opendir(fullpath);
-	if (libraries == 0)
-	{
-		perror("Error loading libraries!");
-		printf("Error number: %d", errno);
-		fflush(stdout);
-		exit(1);
-	}
-
-	while ((dir = readdir(libraries))!=NULL)
-	{
-
-		if (!is_directory(dir->d_name))
-		{
-			/* For some reason, "/plugins" gets detected as a file, so we handle
-			 * this erroneous case
-			 */
-			 if (!strcmp(dir->d_name, "plugins"))
-			 	continue;
-
-
-			char * full_library_path = calloc(strlen(fullpath) +
-			strlen(dir->d_name) + 2,1);
-			strcat(full_library_path, fullpath);
-			strcat(full_library_path, "/");
-			strcat(full_library_path, dir->d_name);
-
-			void * dynamic_library =  dlopen(full_library_path, RTLD_LAZY);
-
-			if (dynamic_library == 0)
-			{
-				printf("%s\n", dir->d_name);
-				dynliberror("Error opening password checker");
-			}
-
-			pwq_t * test_plugin = (pwq_t *)dlsym(dynamic_library, "password_check_plugin");
-
-			password_check = test_plugin->some_password_check;
-			if (!password_check)
-			{
-				dynliberror("Error loading password checker");
-			}
-
-			free(full_library_path);
-
-		}
-	}
-
-	free(fullpath);
+	password_check = password_check_plugin.some_password_check;
 }
 
 int
--- file_locks/file_communication.c
+++ file_locks/file_communication.c
@@ -5,6 +5,11 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <poll.h>
+#include <inttypes.h>
 
 /**
  * The name of the file we will be playing with.
@@ -22,13 +27,17 @@ const char * FILENAME = "random_file";
  * we pre-write all the bytes we need, then lock the parts that we need, rewrite them
  * and unlock them
  */
-const int FILE_LEN = 2000;
+const int FILE_LEN = 4000;
 
 /* The length of the message we will write. If a process fails to write to the file
  * because the other process has locked a part of it, we will simply increment our
  * offset by this size and try and write to the next part!
  */
 const int message_length = sizeof("Hello from process A!\n");
+
+/* Pipe for interprocess communication of the current offset*/
+int pipe_p1_writer[2];
+int pipe_p2_writer[2];
 
 
 void error(const char * message)
@@ -48,6 +57,12 @@ int main(int argc, char * argv[])
 	 * we require, SEEK_SET allows us to input the desired offset*/
 	(void) lseek(fd, SEEK_SET, FILE_LEN-1);
 
+	// Instantiate the pipes for IPC
+	if (pipe(pipe_p1_writer) == -1)
+		error("Pipe 1 error");
+	if (pipe(pipe_p2_writer) == -1)
+		error("Pipe 2 error");
+
 	pid_t pid;
 	if ((pid = fork()) < 0) 
 	{
@@ -56,19 +71,31 @@ int main(int argc, char * argv[])
 
 	if (pid == 0)
 	{
+		// Close the reader part of the pipe, we are only writing to it
+		close(pipe_p1_writer[0]);
+		// Close the writer part of the pipe, we are only reading from it
+		close(pipe_p2_writer[1]);
+
+		FILE * stream = fdopen(pipe_p2_writer[0], "r");
+
+		struct pollfd fds[] =
+		{
+			{ pipe_p2_writer[0], POLLIN}
+		};
+
 		/* Open a new file descriptor for the child, so we don't have two file
 		* descriptors for separate processes and avoid hard to debug errors
 		*/
-	 	 int fd2 = open(FILENAME, O_RDWR);
+	 	int fd2 = open(FILENAME, O_RDWR);
 
-		 if (fd2 < 0)
-		 {
-	 	 	error("Error opening file descriptor child");
-		 }
+		if (fd2 < 0)
+		{
+	 		error("Error opening file descriptor child");
+		}
 
+		off_t offset =0;
 		for (int i = 0; i < 100; i++)
 		{
-
 			 char buf[] = "Hello from process A!\n";
 
 			/* The child locks from the end of the file onwards.
@@ -86,29 +113,86 @@ int main(int argc, char * argv[])
 
 			while ( (block_status = fcntl(fd2, F_SETLK, &f1)) < 0)
 			{
-				//Shift the offset and try to write to the next free location
-				(void)lseek(fd2, SEEK_SET, lseek(fd2, 0, SEEK_CUR) + message_length);
+				if (offset >= FILE_LEN)
+					break;
+
+				/* Wait to get the new offset value from the other process.
+				 * We wait for a total of .5 seconds before going ahead and writing.
+				 * This prevents deadlocks.
+				 */
+				poll(fds, 1, 500);
+				char * offset_buffer = malloc(10);
+	
+				//We timed out on poll- other process has finished
+				if (fds[0].revents != POLLIN)
+					break;
+
+				//Get the current latest offset update from the other file
+				size_t linecapp = 10;
+				if (getline(&offset_buffer,&linecapp, stream) < 0)
+					error("getline error");
+
+				// Shift the offset so we can try to write to the next free location
+				offset = strtol(offset_buffer, NULL, 10);
+
+				(void)lseek(fd2, SEEK_SET, offset);
+				free(offset_buffer);
+
+				printf("Shifted child: %lld\n",offset);
+				fflush(stdout);
 			}
+
+			/* Offset has gone above limit */
+			if (offset >= FILE_LEN)
+				break;
 
 			if (write(fd2, buf, strlen(buf)) < 0)
 			{
 				error("Error writing child process A!");
 			}
 
-			/* Unlock from (SEEK_CUR - strlen(buf)) to SEEK_CUR*/
-			int negative_offset = strlen(buf) * -1;
-			f1.l_start = negative_offset;
-			f1.l_type = F_UNLCK;
-
-			if (fcntl(fd2, F_SETLK, &f1) < 0)
+			/* Tell the other process about the new offest location
+			 * Do this once every 3 writes, to stop excessive updates
+			 */
+			offset += message_length;
+			if (i%3 == 0)
 			{
-				error("Error unlocking file child!");
+				FILE * stream2 = fdopen(pipe_p1_writer[1], "w");
+				fprintf(stream2,"%lld\n", offset);
+				fflush(stream2);
+				printf("Updated parent%lld\n", offset);
 			}
+
+			// /* Unlock from (SEEK_CUR - strlen(buf)) to SEEK_CUR*/
+			// int negative_offset = strlen(buf) * -1;
+			// f1.l_start = negative_offset;
+			// f1.l_type = F_UNLCK;
+
+			// if (fcntl(fd2, F_SETLK, &f1) < 0)
+			// {
+			// 	error("Error unlocking file child!");
+			// }
+			printf("Wrote once child%d\n",i);
+			fflush(stdout);
 		}
 	}
 	else
 	{
-		//Same code and logic, but for process B
+		// Close the writer part of the pipe, we are only reading from it
+		close(pipe_p1_writer[1]);
+		// Close the reader part of the pipe, we are only writing to it
+		close(pipe_p2_writer[0]);
+
+		FILE * stream = fdopen(pipe_p1_writer[0], "r");
+
+		struct pollfd fds[] =
+		{
+			{ pipe_p1_writer[0], POLLIN}
+		};
+
+		off_t offset = 0;
+
+		// Similar code and logic, but for process B
 		for (int i = 0; i < 100; i++)
 		{
 			char buf[] = "Hello from process B!\n";
@@ -124,23 +208,67 @@ int main(int argc, char * argv[])
 
 			while ( (block_status = fcntl(fd, F_SETLK, &f1)) < 0)
 			{
-				//Shift the offset and try to write to the next free location
-				(void)lseek(fd, SEEK_SET, lseek(fd, 0, SEEK_CUR) + message_length);
+				// Shift the offset and try to write to the next free location
+				if (offset >= FILE_LEN)
+					break;
+
+				// Wait to get the offset from the other process
+				poll(fds,1,500);
+				char * offset_buffer = malloc(10);
+
+				// We timed out on poll- other process has finished
+				if (fds[0].revents != POLLIN)
+					break;
+
+				size_t linecapp = 10;
+				if (getline(&offset_buffer,&linecapp, stream) < 0)
+					error("getline error");
+
+				// Shift the offset and try to write to the next free location
+				offset = strtol(offset_buffer, NULL, 10);
+				// offset += (lseek(fd, 0, SEEK_CUR) + message_length);
+				(void)lseek(fd, SEEK_SET, offset);
+
+				free(offset_buffer);
+				printf("Shifted parent:%lld\n",offset);
+				fflush(stdout);
 			}
+
+			if (offset >= FILE_LEN)
+				break;
 
 			if (write(fd, buf, strlen(buf)) < 0)
 			{
 				error("Error writing parent process B!");
 			}
 
-			/* Unlock from (SEEK_CUR - strlen(buf)) to SEEK_CUR*/
-			int negative_offset = strlen(buf) * -1;
-			f1.l_start = negative_offset;
-			f1.l_type = F_UNLCK;
-			if (fcntl(fd, F_SETLK, &f1) < 0)
+			/* Tell the other process about the new offest location
+			 * Do this once every 3 writes, to stop excessive updates
+			 */
+			offset += message_length;
+			if (i%3 == 0)
 			{
-				error("Error unlocking file parent!");
+				FILE * stream2 = fdopen(pipe_p2_writer[1], "w");
+				fprintf(stream2,"%lld\n", offset);
+				fflush(stream2);
+				printf("Updated child%lld\n", offset);
 			}
+
+
+			// /* Unlock from (SEEK_CUR - strlen(buf)) to SEEK_CUR*/
+			// int negative_offset = strlen(buf) * -1;
+			// f1.l_start = negative_offset;
+			// f1.l_type = F_UNLCK;
+			// if (fcntl(fd, F_SETLK, &f1) < 0)
+			// {
+			// 	error("Error unlocking file parent!");
+			// }
+			printf("Wrote once parent%d\n", i);
+			fflush(stdout);
 		}
+
+		//Wait for child to finish so parent's locks persist
+		int status;
+		wait(&status);
 	}
 }
